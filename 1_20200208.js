9333c93cf0e6b5012e92f4bebc70934ac9b53d90
kyunghoonKim <kimkyonghun@gmail.com>
2020-02-08 21:02:57 +0900

    1단계: DOM가져오기_1st

---
+++ 1_20200208.js
@@ -0,0 +1,11 @@
+// const divSelector = 'div'
+// const div = document.querySelector(divSelector)
+
+// const buttonSelector = 'button'
+// const button = document.querySelector(buttonSelector)
+
+// const childButtonSelector = 'button'
+// const childButton = div.querySelector(childButtonSelector)
+
+const selector = 'button'
+const buttons = document.querySelectorAll(selector)
